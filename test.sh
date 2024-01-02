56a7b788a6d783e21f1fb7a28d4139964d201c91
Dorian <dorian@resalocal.com>
2024-01-02 23:30:08 +0100

    Sandbox env

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-zig11 build && ./zig-out/bin/wool ./test.wool && wat2wasm out.wat
+zig11 build && ./zig-out/bin/wool ./test.wool && wat2wasm out.wat   && mv out.wasm test/test.wasm  && bun run ./test/index.js
--- test/index.js
+++ test/index.js
@@ -1,11 +1,13 @@
 const dir = import.meta.dir;
 
 // Fetch the WebAssembly file
-const src = Bun.file(`${dir}/out.wasm`);
+const src = Bun.file(`${dir}/test.wasm`);
 const bytes = await src.arrayBuffer();
 
 // Create a WebAssembly instance
 const {instance, module} = await WebAssembly.instantiate(bytes);
+
+console.log("Running exported WASM function from Bun");
 
 // Call the exported function
 const result = instance.exports.add(10, 20);
