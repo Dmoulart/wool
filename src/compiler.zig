76ed7a9469d04adf281bb08a380df080b8d5b6fd
_aion <dorian.molart@gmail.com>
2024-02-25 14:31:27 +0100

    Setting binaryen namespace in compiler

--- src/compiler.zig
+++ src/compiler.zig
@@ -1,6 +1,6 @@
 allocator: std.mem.Allocator,
 ir: []Ir.Inst,
-module: c.BinaryenModuleRef,
+binaryen: Binaryen,
 
 const CompileError = error{NotImplemented};
 
@@ -8,7 +8,9 @@ pub fn init(allocator: std.mem.Allocator, ir: []Ir.Inst) Compiler {
     return .{
         .allocator = allocator,
         .ir = ir,
-        .module = c.BinaryenModuleCreate(),
+        .binaryen = Binaryen.init(
+            c.BinaryenModuleCreate(),
+        ),
     };
 }
 
@@ -22,12 +24,10 @@ pub fn compile(self: *Compiler, inst: *Ir.Inst) !void {
     switch (inst.*) {
         // .push_i32 => |*push_i32| c.BinaryenConst(self.module, c.BinaryenLiteralInt32(push_i32)),
         .global_i32 => |*global_i32| {
-            _ = c.BinaryenAddGlobal(
-                self.module,
+            _ = self.binaryen.add_immutable_global(
                 self.to_c_str(global_i32.name),
-                c.BinaryenTypeInt32(),
-                false,
-                c.BinaryenConst(self.module, c.BinaryenLiteralInt32(global_i32.value)),
+                .i32,
+                global_i32.value,
             );
         },
         else => return CompileError.NotImplemented,
@@ -35,11 +35,11 @@ pub fn compile(self: *Compiler, inst: *Ir.Inst) !void {
 }
 
 pub fn write(self: *Compiler) !void {
-    c.BinaryenModulePrint(self.module);
+    c.BinaryenModulePrint(self.binaryen.module);
 
     var buffer: [10_000]u8 = undefined;
 
-    const out = c.BinaryenModuleAllocateAndWriteText(self.module);
+    const out = c.BinaryenModuleAllocateAndWriteText(self.binaryen.module);
 
     const code = try std.fmt.bufPrintZ(&buffer, "{s}", .{out});
 
@@ -60,9 +60,85 @@ fn to_c_str(self: *Compiler, str: []const u8) [*:0]const u8 {
     return @ptrCast(self.allocator.dupeZ(u8, str) catch unreachable);
 }
 
+const Binaryen = struct {
+    module: c.BinaryenModuleRef,
+
+    pub fn init(module: c.BinaryenModuleRef) Binaryen {
+        return .{
+            .module = module,
+        };
+    }
+
+    pub fn constant(self: *Binaryen, comptime tid: Infer.TypeID, value: anytype) c.BinaryenExpressionRef {
+        return c.BinaryenConst(self.module, self.literal(tid, value));
+        // return c.BinaryenAddGlobal(
+        //     self.module,
+        //     self.to_c_str(global_i32.name),
+        //     Bin.ty(.i32),
+        //     false,
+        //     c.BinaryenConst(self.module, Bin.literal(.i32, global_i32.value)),
+        // );
+    }
+
+    pub fn add_immutable_global(
+        self: *Binaryen,
+        name: [*:0]const u8,
+        comptime tid: Infer.TypeID,
+        value: anytype,
+    ) c.BinaryenGlobalRef {
+        return c.BinaryenAddGlobal(
+            self.module,
+            name,
+            self.ty(tid),
+            false,
+            self.constant(tid, value),
+        );
+    }
+
+    pub fn add_mutable_global(
+        self: *Binaryen,
+        name: [*:0]const u8,
+        comptime tid: Infer.TypeID,
+        value: anytype,
+    ) c.BinaryenExpressionRef {
+        return c.BinaryenAddGlobal(
+            self.module,
+            name,
+            self.ty(tid),
+            true,
+            self.constant(tid, value),
+        );
+    }
+
+    pub fn ty(self: *Binaryen, comptime tid: Infer.TypeID) c.BinaryenType {
+        _ = self;
+        return switch (tid) {
+            .i32, .bool => c.BinaryenTypeInt32(),
+            .i64 => c.BinaryenTypeInt64(),
+            .f32 => c.BinaryenTypeFloat32(),
+            .f64 => c.BinaryenTypeFloat64(),
+            .void => c.BinaryenTypeNone(),
+            else => unreachable,
+        };
+    }
+
+    pub fn literal(self: *Binaryen, comptime tid: Infer.TypeID, value: anytype) c.BinaryenLiteral {
+        _ = self;
+
+        return switch (tid) {
+            .i32, .bool => c.BinaryenLiteralInt32(value),
+            .i64 => c.BinaryenLiteralInt64(value),
+            .f32 => c.BinaryenLiteralFloat32(value),
+            .f64 => c.BinaryenLiteralFloat64(value),
+            else => unreachable,
+        };
+    }
+};
+
 const std = @import("std");
 const c = @cImport({
     @cInclude("binaryen-c.h");
 });
 const Compiler = @This();
+const Infer = @import("./infer.zig");
 const Ir = @import("./ir.zig");
