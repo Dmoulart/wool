1e78ca8c75796f208dd5cc4a3ec54990c97cbd60
_aion <dorian.molart@gmail.com>
2024-03-03 17:44:29 +0100

    Add Logical !

--- src/ir.zig
+++ src/ir.zig
@@ -167,25 +167,34 @@ pub fn emit(self: *Ir, inst: *Inst) !void {
 
 pub fn convert(self: *Ir, sem: *Infer.Sem) anyerror!*Inst {
     switch (sem.*) {
-        .Literal => |*literal| return switch (get_sem_tid(sem)) {
-            //temporary, Number should be narrowed in Infer phase or an error should be thrown
-            .number, .i32 => try self.create_inst(.{
-                .value_i32 = try std.fmt.parseInt(i32, literal.orig_expr.Literal.value.Number, 10),
-            }),
-            .i64 => try self.create_inst(.{
-                .value_i64 = try std.fmt.parseInt(i64, literal.orig_expr.Literal.value.Number, 10),
-            }),
-            .float, .f32 => try self.create_inst(.{
-                .value_f32 = try std.fmt.parseFloat(f32, literal.orig_expr.Literal.value.Number),
-            }),
-            .f64 => try self.create_inst(.{
-                .value_f64 = try std.fmt.parseFloat(f64, literal.orig_expr.Literal.value.Number),
-            }),
-            .bool => try self.create_inst(.{
-                .value_bool = if (literal.orig_expr.Literal.value.Boolean) 1 else 0,
-            }),
-            else => IrError.NotImplemented,
+        .Literal => |*lit| {
+            const tid = get_sem_tid(sem);
+            // const lit_inst = switch (tid) {
+            //     .bool => try literal(tid, lit.orig_expr.Literal.value.Boolean),
+            //     .number, .i32, .i64, .float, .f32, .f64 => try literal(tid, lit.orig_expr.Literal.value.Number),
+            //     else => unreachable,
+            // };
+            return try self.create_inst(try literal(tid, lit.orig_expr.Literal.value));
         },
+        // .Literal => |*lit| return switch (get_sem_tid(sem)) {
+        //     //temporary, Number should be narrowed in Infer phase or an error should be thrown
+        //     .number, .i32 => try self.create_inst(.{
+        //         .value_i32 = try std.fmt.parseInt(i32, literal.orig_expr.Literal.value.Number, 10),
+        //     }),
+        //     .i64 => try self.create_inst(.{
+        //         .value_i64 = try std.fmt.parseInt(i64, literal.orig_expr.Literal.value.Number, 10),
+        //     }),
+        //     .float, .f32 => try self.create_inst(.{
+        //         .value_f32 = try std.fmt.parseFloat(f32, literal.orig_expr.Literal.value.Number),
+        //     }),
+        //     .f64 => try self.create_inst(.{
+        //         .value_f64 = try std.fmt.parseFloat(f64, literal.orig_expr.Literal.value.Number),
+        //     }),
+        //     .bool => try self.create_inst(.{
+        //         .value_bool = if (literal.orig_expr.Literal.value.Boolean) 1 else 0,
+        //     }),
+        //     else => IrError.NotImplemented,
+        // },
         .VarInit => |*var_init| {
             const name = var_init.orig_expr.VarInit.name.lexeme;
             const tid = get_sem_tid(as_sem(var_init.initializer));
@@ -302,181 +311,16 @@ pub fn convert(self: *Ir, sem: *Infer.Sem) anyerror!*Inst {
                 },
             );
         },
-        .Binary => |*binary| {
-            const left = try self.convert(as_sem(binary.left));
-            const right = try self.convert(as_sem(binary.right));
+        .Binary => |*bin| {
+            const left = try self.convert(as_sem(bin.left));
+            const right = try self.convert(as_sem(bin.right));
 
-            const op = binary.orig_expr.Binary.op.type;
+            const op = bin.orig_expr.Binary.op.type;
 
             // We could have picked binary.right.
-            const tid = get_sem_tid(as_sem(binary.left));
+            const tid = get_sem_tid(as_sem(bin.left));
 
-            const values = .{
-                .left = left,
-                .right = right,
-            };
-
-            const inst: Ir.Inst = switch (op) {
-                .PLUS => switch (tid) {
-                    .number, .i32 => .{
-                        .add_i32 = values,
-                    },
-                    .i64 => .{
-                        .add_i64 = values,
-                    },
-                    .float, .f32 => .{
-                        .add_f32 = values,
-                    },
-                    .f64 => .{
-                        .add_f64 = values,
-                    },
-                    else => unreachable,
-                },
-                .MINUS => switch (tid) {
-                    .number, .i32 => .{
-                        .sub_i32 = values,
-                    },
-                    .i64 => .{
-                        .sub_i64 = values,
-                    },
-                    .float, .f32 => .{
-                        .sub_f32 = values,
-                    },
-                    .f64 => .{
-                        .sub_f64 = values,
-                    },
-                    else => unreachable,
-                },
-                .STAR => switch (tid) {
-                    .number, .i32 => .{
-                        .mul_i32 = values,
-                    },
-                    .i64 => .{
-                        .mul_i64 = values,
-                    },
-                    .float, .f32 => .{
-                        .mul_f32 = values,
-                    },
-                    .f64 => .{
-                        .mul_f64 = values,
-                    },
-                    else => unreachable,
-                },
-                .SLASH => switch (tid) {
-                    .number, .i32 => .{
-                        .div_i32 = values,
-                    },
-                    .i64 => .{
-                        .div_i64 = values,
-                    },
-                    .float, .f32 => .{
-                        .div_f32 = values,
-                    },
-                    .f64 => .{
-                        .div_f64 = values,
-                    },
-                    else => unreachable,
-                },
-                .EQUAL_EQUAL => switch (tid) {
-                    .bool => .{
-                        .eq_bool = values,
-                    },
-                    .number, .i32 => .{
-                        .eq_i32 = values,
-                    },
-                    .i64 => .{
-                        .eq_i64 = values,
-                    },
-                    .float, .f32 => .{
-                        .eq_f32 = values,
-                    },
-                    .f64 => .{
-                        .eq_f64 = values,
-                    },
-                    else => unreachable,
-                },
-                .BANG_EQUAL => switch (tid) {
-                    .bool => .{
-                        .neq_bool = values,
-                    },
-                    .number, .i32 => .{
-                        .neq_i32 = values,
-                    },
-                    .i64 => .{
-                        .neq_i64 = values,
-                    },
-                    .float, .f32 => .{
-                        .neq_f32 = values,
-                    },
-                    .f64 => .{
-                        .neq_f64 = values,
-                    },
-                    else => unreachable,
-                },
-                .GREATER => switch (tid) {
-                    .number, .i32 => .{
-                        .gt_i32 = values,
-                    },
-                    .i64 => .{
-                        .gt_i64 = values,
-                    },
-                    .float, .f32 => .{
-                        .gt_f32 = values,
-                    },
-                    .f64 => .{
-                        .gt_f64 = values,
-                    },
-                    else => unreachable,
-                },
-                .GREATER_EQUAL => switch (tid) {
-                    .number, .i32 => .{
-                        .ge_i32 = values,
-                    },
-                    .i64 => .{
-                        .ge_i64 = values,
-                    },
-                    .float, .f32 => .{
-                        .ge_f32 = values,
-                    },
-                    .f64 => .{
-                        .ge_f64 = values,
-                    },
-                    else => unreachable,
-                },
-                .LESS => switch (tid) {
-                    .number, .i32 => .{
-                        .lt_i32 = values,
-                    },
-                    .i64 => .{
-                        .lt_i64 = values,
-                    },
-                    .float, .f32 => .{
-                        .lt_f32 = values,
-                    },
-                    .f64 => .{
-                        .lt_f64 = values,
-                    },
-                    else => unreachable,
-                },
-                .LESS_EQUAL => switch (tid) {
-                    .number, .i32 => .{
-                        .le_i32 = values,
-                    },
-                    .i64 => .{
-                        .le_i64 = values,
-                    },
-                    .float, .f32 => .{
-                        .le_f32 = values,
-                    },
-                    .f64 => .{
-                        .le_f64 = values,
-                    },
-                    else => unreachable,
-                },
-                else => return IrError.NotImplemented,
-            };
-
-            return try self.create_inst(inst);
+            return try self.create_inst(try binary(tid, op, left, right));
         },
         .Variable => |*variable| {
             const name = variable.orig_expr.Variable.name.lexeme;
@@ -506,27 +350,43 @@ pub fn convert(self: *Ir, sem: *Infer.Sem) anyerror!*Inst {
             return IrError.NotImplemented;
         },
         .Logical => |logical| {
-            // const tid = get_sem_tid(sem);
+            const tid = get_sem_tid(sem);
 
             const left = try self.convert(as_sem(logical.left));
             const right = try self.convert(as_sem(logical.right));
 
+            const op = logical.orig_expr.Logical.op.type;
+
+            const bin = try self.create_inst(try binary(
+                tid,
+                if (op == .AND) .GREATER else .EQUAL_EQUAL,
+                left,
+                try self.create_inst(try literal(tid, if (tid == .bool) Expr.Literal.Value{
+                    .Boolean = true, //@todo  this sucks
+                } else Expr.Literal.Value{
+                    .Number = "0",
+                })),
+            ));
+
             //@todo refacto with if
-            const values = switch (logical.orig_expr.Logical.op.type) {
-                .AND => .{
-                    .condition = left,
-                    .then_branch = right,
-                    .else_branch = left,
-                },
-                .OR => .{
-                    .condition = left,
-                    .then_branch = left,
-                    .else_branch = right,
-                },
+            const values = .{
+                .condition = bin,
+                .then_branch = right,
+                .else_branch = left,
+            };
+
+            const select_inst: Inst = switch (tid) {
+                .bool => .{ .select_bool = values },
+                .i32 => .{ .select_i32 = values },
+                .i64 => .{ .select_i64 = values },
+                .f32 => .{ .select_f32 = values },
+                .f64 => .{ .select_f64 = values },
                 else => unreachable,
             };
 
-            return try self.create_inst(.{ .@"if" = values });
+            return try self.create_inst(select_inst);
+
+            // return try self.create_inst(.{ .@"if" = values });
 
             // const select_inst: Inst = switch (tid) {
             //     .bool => .{ .select_bool = values },
@@ -586,6 +446,201 @@ pub fn convert(self: *Ir, sem: *Infer.Sem) anyerror!*Inst {
     }
 }
 
+fn literal(tid: Infer.TypeID, value: Expr.Literal.Value) !Inst {
+    return switch (tid) {
+        //temporary, Number should be narrowed in Infer phase or an error should be thrown
+        .number, .i32 => .{
+            .value_i32 = try std.fmt.parseInt(i32, value.Number, 10),
+        },
+        .i64 => .{
+            .value_i64 = try std.fmt.parseInt(i64, value.Number, 10),
+        },
+        .float, .f32 => .{
+            .value_f32 = try std.fmt.parseFloat(f32, value.Number),
+        },
+        .f64 => .{
+            .value_f64 = try std.fmt.parseFloat(f64, value.Number),
+        },
+        .bool => .{
+            .value_bool = if (value.Boolean) 1 else 0,
+        },
+        else => IrError.NotImplemented,
+    };
+}
+fn binary(
+    tid: Infer.TypeID,
+    op: Token.Type,
+    left: *Inst,
+    right: *Inst,
+) !Inst {
+    const values = .{
+        .left = left,
+        .right = right,
+    };
+
+    const inst: Ir.Inst = switch (op) {
+        .PLUS => switch (tid) {
+            .number, .i32 => .{
+                .add_i32 = values,
+            },
+            .i64 => .{
+                .add_i64 = values,
+            },
+            .float, .f32 => .{
+                .add_f32 = values,
+            },
+            .f64 => .{
+                .add_f64 = values,
+            },
+            else => unreachable,
+        },
+        .MINUS => switch (tid) {
+            .number, .i32 => .{
+                .sub_i32 = values,
+            },
+            .i64 => .{
+                .sub_i64 = values,
+            },
+            .float, .f32 => .{
+                .sub_f32 = values,
+            },
+            .f64 => .{
+                .sub_f64 = values,
+            },
+            else => unreachable,
+        },
+        .STAR => switch (tid) {
+            .number, .i32 => .{
+                .mul_i32 = values,
+            },
+            .i64 => .{
+                .mul_i64 = values,
+            },
+            .float, .f32 => .{
+                .mul_f32 = values,
+            },
+            .f64 => .{
+                .mul_f64 = values,
+            },
+            else => unreachable,
+        },
+        .SLASH => switch (tid) {
+            .number, .i32 => .{
+                .div_i32 = values,
+            },
+            .i64 => .{
+                .div_i64 = values,
+            },
+            .float, .f32 => .{
+                .div_f32 = values,
+            },
+            .f64 => .{
+                .div_f64 = values,
+            },
+            else => unreachable,
+        },
+        .EQUAL_EQUAL => switch (tid) {
+            .bool => .{
+                .eq_bool = values,
+            },
+            .number, .i32 => .{
+                .eq_i32 = values,
+            },
+            .i64 => .{
+                .eq_i64 = values,
+            },
+            .float, .f32 => .{
+                .eq_f32 = values,
+            },
+            .f64 => .{
+                .eq_f64 = values,
+            },
+            else => unreachable,
+        },
+        .BANG_EQUAL => switch (tid) {
+            .bool => .{
+                .neq_bool = values,
+            },
+            .number, .i32 => .{
+                .neq_i32 = values,
+            },
+            .i64 => .{
+                .neq_i64 = values,
+            },
+            .float, .f32 => .{
+                .neq_f32 = values,
+            },
+            .f64 => .{
+                .neq_f64 = values,
+            },
+            else => unreachable,
+        },
+        .GREATER => switch (tid) {
+            .number, .i32 => .{
+                .gt_i32 = values,
+            },
+            .i64 => .{
+                .gt_i64 = values,
+            },
+            .float, .f32 => .{
+                .gt_f32 = values,
+            },
+            .f64 => .{
+                .gt_f64 = values,
+            },
+            else => unreachable,
+        },
+        .GREATER_EQUAL => switch (tid) {
+            .number, .i32 => .{
+                .ge_i32 = values,
+            },
+            .i64 => .{
+                .ge_i64 = values,
+            },
+            .float, .f32 => .{
+                .ge_f32 = values,
+            },
+            .f64 => .{
+                .ge_f64 = values,
+            },
+            else => unreachable,
+        },
+        .LESS => switch (tid) {
+            .number, .i32 => .{
+                .lt_i32 = values,
+            },
+            .i64 => .{
+                .lt_i64 = values,
+            },
+            .float, .f32 => .{
+                .lt_f32 = values,
+            },
+            .f64 => .{
+                .lt_f64 = values,
+            },
+            else => unreachable,
+        },
+        .LESS_EQUAL => switch (tid) {
+            .number, .i32 => .{
+                .le_i32 = values,
+            },
+            .i64 => .{
+                .le_i64 = values,
+            },
+            .float, .f32 => .{
+                .le_f32 = values,
+            },
+            .f64 => .{
+                .le_f64 = values,
+            },
+            else => unreachable,
+        },
+        else => return IrError.NotImplemented,
+    };
+
+    return inst;
+}
+
 fn find_str(item: []const u8, target: []const u8) bool {
     return std.mem.eql(u8, item, target);
 }
@@ -598,17 +653,17 @@ fn create_inst(self: *Ir, inst: Inst) !*Inst {
 
 fn eval(self: *Ir, sem: *Infer.Sem, comptime InputType: type, comptime ReturnType: type) !ReturnType {
     return switch (sem.*) {
-        .Literal => |*literal| switch (InputType) {
-            bool => if (literal.orig_expr.Literal.value.Boolean) 1 else 0,
-            i32, i64 => try std.fmt.parseInt(InputType, literal.orig_expr.Literal.value.Number, 10),
-            f32, f64 => try std.fmt.parseFloat(InputType, literal.orig_expr.Literal.value.Number),
+        .Literal => |*lit| switch (InputType) {
+            bool => if (lit.orig_expr.Literal.value.Boolean) 1 else 0,
+            i32, i64 => try std.fmt.parseInt(InputType, lit.orig_expr.Literal.value.Number, 10),
+            f32, f64 => try std.fmt.parseFloat(InputType, lit.orig_expr.Literal.value.Number),
             else => IrError.NotImplemented,
         },
-        .Binary => |*binary| blk: {
-            const left = try self.eval(as_sem(binary.left), InputType, ReturnType);
-            const right = try self.eval(as_sem(binary.right), InputType, ReturnType);
+        .Binary => |*bin| blk: {
+            const left = try self.eval(as_sem(bin.left), InputType, ReturnType);
+            const right = try self.eval(as_sem(bin.right), InputType, ReturnType);
 
-            break :blk switch (binary.orig_expr.Binary.op.type) {
+            break :blk switch (bin.orig_expr.Binary.op.type) {
                 .PLUS => left + right,
                 .MINUS => left - right,
                 .STAR => left * right,
@@ -629,7 +684,9 @@ fn eval(self: *Ir, sem: *Infer.Sem, comptime InputType: type, comptime ReturnTyp
 const std = @import("std");
 const meta = std.meta;
 const activeTag = std.meta.activeTag;
+const Token = @import("./token.zig");
 const Infer = @import("./infer.zig");
+const Expr = @import("./ast/expr.zig").Expr;
 const as_sem = @import("./infer.zig").as_sem;
 const get_sem_tid = @import("./infer.zig").get_sem_tid;
 
